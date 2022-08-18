b64b432e3fe2bcd03a20ddc8d6b9b60d41122bab
MP <cmp1@deloitte.com>
2022-08-18 13:28:20 +0530

    base setup

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/po")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+from flask import url_for
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
